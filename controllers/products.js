f49a6c482b98370d003b50d722fdbfeb929ffa02
shmulik.reihani <shmulik1102@gmail.com>
2021-08-24 16:46:21 +0300

    product routes

--- controllers/products.js
+++ controllers/products.js
@@ -23,3 +23,63 @@ exports.deleteProduct = (req, res, next) => {
     res.status(200).send(products);
   });
 };
+
+exports.getAllBySearch = (req, res, next) => {
+  const { title, category, description, maxPrice, minPrice } = req.query;
+  let query = {};
+  if (title) {
+    query["title"] = { $regex: title, $options: "i" };
+  }
+  if (category) {
+    query["category"] = { $regex: category, $options: "i" };
+  }
+  if (description) {
+    query["description"] = { $regex: category, $options: "i" };
+  }
+  if (maxPrice && minPrice) {
+    query["price"] = { $lte: maxPrice, $gte: minPrice };
+  }
+  Product.find(query, (err, products) => {
+    if (err) res.status(400).json(err);
+    return res.status(200).send(products);
+  });
+};
+
+exports.createNewProduct = (req, res, next) => {
+  const { title, category, description, price, image } = req.body;
+
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    category: category,
+    image: image,
+  });
+
+  product.save((err) => {
+    if (err) res.status(400).json(err);
+    return res.status(200).send("OK");
+  });
+};
+
+exports.updateSingleProduct = async (req, res, next) => {
+  const { id } = req.params.id;
+  const { title, price, description, category, image } = req.body;
+
+  try {
+    var updatedProduct = await Product.findByIdAndUpdate(id, {
+      title,
+      price,
+      description,
+      category,
+      image,
+    });
+    return res.status(200).json({
+      status: 200,
+      data: updatedProduct,
+      message: "Succesfully update post",
+    });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,10 @@ const route = express.Router();
 const productsController = require("../controllers/products");
 
 route.get("/", productsController.getAllProducts);
+route.post("/", productsController.createNewProduct);
+route.get("/search", productsController.getAllBySearch);
 route.get("/:id", productsController.getSingleProduct);
+route.put("/:id", productsController.updateSingleProduct);
 route.delete("/:id", productsController.deleteProduct);
 
 module.exports = route;
