6c968695004d16eb6fc16289026223cfc489a70a
shmulik.reihani <shmulik1102@gmail.com>
2021-08-31 17:09:39 +0300

    chaned

--- app.js
+++ app.js
@@ -11,10 +11,20 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+mongoose.connect(
+  process.env.DATABASE,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw Error(err);
+    app.listen(8000, () => {
+      console.log(`YOU CONNECT TO http://localhost:${PORT}`);
+    });
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -23,6 +33,6 @@ app.get("/", (req, res) => {
 app.use("/api", require("./api/index"));
 setup(app);
 
-app.listen(8000, () => {
-  console.log(`YOU CONNECT TO http://localhost:${PORT}`);
-});
+// app.listen(8000, () => {
+//   console.log(`YOU CONNECT TO http://localhost:${PORT}`);
+// });
