bdcf6ee632f2fff77c14f0b467c83efd63840ffc
shmulik.reihani <shmulik1102@gmail.com>
2021-08-31 20:11:20 +0300

    changed for heroku

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use("/", express.static("client"));
+const url = process.env.MONGO_URI || process.env.DATABASE;
+
 mongoose.connect(
-  process.env.DATABASE,
+  url,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
