2b655adc8ffc459a8ac6b7770592b06653ff8494
shmulik.reihani <shmulik1102@gmail.com>
2021-09-01 09:09:49 +0300

    deploy

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/", express.static("client"));
+// app.use("/", express.static("client"));
 const url = process.env.MONGO_URI || process.env.DATABASE;
 
 mongoose.connect(
